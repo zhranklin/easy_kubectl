95f6a263175f2ca25dd282795ec68f07df6b3906
Zhranklin <zhangwu@corp.netease.com>
2020-12-23 16:44:06 +0800

    fix

--- install.sh
+++ install.sh
@@ -58,11 +58,11 @@ function k() {
       fi
     done
     QUERY=""
+    word=$(echo $1|sed -r 's/([A-Z])$/\l\1./g; s/^([A-Z])/.\l\1/g; s/([a-zA-Z])[\[\.:,]$/\l\1./g; s/^[\[\.:,]([a-zA-Z])/.\l\1/g;')
+    GREP_PREFIX="grep -iE ^$(echo $word|sed -nr 's/^\.([a-zA-Z]).*/\l\1/p')"
+    GREP_POSTFIX="grep -iE $(echo $word|sed -nr 's/.*([a-zA-Z])\.$/\l\1/p')\$"
+    word=$(echo $word|sed 's/\.$//g; s/^\.//g')
     if [[ -n $1 ]]; then
-      word=$(echo $1|sed -r 's/([A-Z])$/\l\1./g; s/^([A-Z])/.\l\1/g; s/([a-zA-Z])[\[\.:,]$/\l\1./g; s/^[\[\.:,]([a-zA-Z])/.\l\1/g;')
-      GREP_PREFIX="grep -iE ^$(echo $word|sed -nr 's/^\.([a-zA-Z]).*/\l\1/p')"
-      GREP_POSTFIX="grep -iE $(echo $word|sed -nr 's/.*([a-zA-Z])\.$/\l\1/p')\$"
-      word=$(echo $word|sed 's/\.$//g; s/^\.//g')
       QUERY="--query=$word -1 -0"
     fi
     NEW_NS=$(echo "$NS_RESULT"|tr ' ' '\n'|$GREP_PREFIX|$GREP_POSTFIX|$HOME/.easy_kubectl/fzf --prompt="search for namespace: " --tiebreak=end,index -i $QUERY)
